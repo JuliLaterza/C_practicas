18f783b0728702753ad7c429093919df276e0034
Julián Laterza <laterzajulian@gmail.com>
2024-03-13 22:42:26 -0300

    Agrupacion Modulo 1

--- Modulo 1/modulo1a.c
+++ Modulo 1/modulo1a.c
@@ -2,7 +2,7 @@
 
 int main() {
     int cant_num_malvados = 0; int numero = 0;
-    float promedio = 2.5;
+    float promedio = 2;
     while (cant_num_malvados < 4) {
         if (numero % 2 == 0 || numero % 3 == 0 || numero % 5 == 0 || numero % 7 == 0) {
             cant_num_malvados++;
