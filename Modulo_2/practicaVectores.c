95ef851581436594282ae8fe03b8fe7334815ee9
Julián Laterza <laterzajulian@gmail.com>
2024-03-17 17:55:59 -0300

    update PracticaVectores

--- Modulo_2/practicaVectores.c
+++ Modulo_2/practicaVectores.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
 
 int main() {
-    int vec[3];
-    int i = 0;
 
-    for (i = 0; i < 3; i++) {
+    int n;
+    int i = 0, sum = 0;
+    
+    printf("Ingrese la cantidad de elementos: \n");
+    scanf("%d", &n);
+    int vec[n];
+    
+    for (i = 0; i < n ; i++) {
         printf("Ingrese un numero: \n");
         scanf("%d", &vec[i]);
     }
+    
     printf("\n");
-    for (i = 0; i < 3; i++) {
-        printf("%d\n", vec[i]);
+    for (i = 0; i < n; i++) {
+        sum += vec[i];
     }
+
+    // La diferencia entre mostrar y asignar es que para asignar es con punteros
+    for (i = 0; i < n; i++) {
+        printf("vec[%d] = %d\n",i, vec[i]); // aca accedemos al indice del vector e imprimimos
+    }
+
+    printf("\nLa sumatoria total del vector es %d", sum);
     return 0;
 }
